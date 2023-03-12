6fe977bdaac2c07c78727c1dce80462b50af91e0
Ivo G <41997352+Dadogg80@users.noreply.github.com>
2023-03-12 11:25:23 +0000

    feat: add button component

---
+++ personality_test/src/components/Button/Button.module.tsx
@@ -0,0 +1,33 @@
+import styled, {css} from 'styled-components';
+import { render } from '@testing-library/react';
+
+const Button = styled.button` 
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: #db9827;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+
+
+    ${props =>
+        props.primary &&
+        css`
+          background: palevioletred;
+          color: white;
+    `};
+`;
+
+
+const Container = styled.div`
+  text-align: center;
+`
+
+render(
+    <Container>
+      <Button>Normal Button</Button>
+      <Button primary>Primary Button</Button>
+    </Container>
+);
+
+export default Button;
